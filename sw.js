edf0c06dd5ed23f78ae9644a8eb824483ea76839
Abdullah Alshehri <alshehri.a.j@gmail.com>
2019-12-05 17:58:39 +0300

    service worker activation

--- sw.js
+++ sw.js
@@ -27,3 +27,18 @@ self.addEventListener('install', function(event) {
       })
     );
   });
+
+
+  self.addEventListener('activate', function(event) {
+    event.waitUntil(
+      caches.keys().then(function(cacheNames) {
+        return Promise.all(
+          cacheNames.map(function(cache) {
+            if (cache !== cacheName) {
+                caches.delete(cache);
+            }
+          })
+        );
+      })
+    );
+  });
