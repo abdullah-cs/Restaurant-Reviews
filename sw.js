244c1bab85b5529af8734a58f047872b25630840
Abdullah Alshehri <alshehri.a.j@gmail.com>
2019-12-05 20:54:38 +0300

    hijack fetch requests

--- sw.js
+++ sw.js
@@ -2,43 +2,65 @@
 const cacheName = 'RESTAURANT-REVIEWS-V1';
 
 
-self.addEventListener('install', function(event) {
+self.addEventListener('install', function (event) {
     event.waitUntil(
-      caches.open(cacheName).then(function(cache) {
-        return cache.addAll([
-          'index.html',
-          'restaurant.html',
-          'css/styles.css',
-          'js/dbhelper.js',
-          'js/main.js',
-          'js/restaurant_info.js',
-          'data/restaurants.json',
-          'img/1.jpg',
-          'img/2.jpg',
-          'img/3.jpg',
-          'img/4.jpg',
-          'img/5.jpg',
-          'img/6.jpg',
-          'img/7.jpg',
-          'img/8.jpg',
-          'img/9.jpg',
-          'img/10.jpg'
-        ]);
-      })
+        caches.open(cacheName)
+        .then(function (cache) {
+             cache.addAll([
+                'index.html',
+                'restaurant.html',
+                'css/styles.css',
+                'js/dbhelper.js',
+                'js/main.js',
+                'js/restaurant_info.js',
+                'data/restaurants.json',
+                'img/1.jpg',
+                'img/2.jpg',
+                'img/3.jpg',
+                'img/4.jpg',
+                'img/5.jpg',
+                'img/6.jpg',
+                'img/7.jpg',
+                'img/8.jpg',
+                'img/9.jpg',
+                'img/10.jpg'
+            ]).then( function () {
+                self.skipWaiting();
+            });
+        })
     );
-  });
+});
 
 
-  self.addEventListener('activate', function(event) {
+self.addEventListener('activate', function (event) {
     event.waitUntil(
-      caches.keys().then(function(cacheNames) {
-        return Promise.all(
-          cacheNames.map(function(cache) {
-            if (cache !== cacheName) {
-                caches.delete(cache);
-            }
-          })
-        );
-      })
+        caches.keys()
+        .then(function (cacheNames) {
+            return Promise.all(
+                cacheNames.map(function (cache) {
+                    if (cache !== cacheName) {
+                         return caches.delete(cache);
+                    }
+                })
+            );
+        })
     );
-  });
+});
+
+
+self.addEventListener('fetch', event => {
+
+    event.respondWith(
+
+        fetch(event.request)
+        .then( res => {
+            const clone = res.clone();
+
+            caches.open(cacheName)
+            .then(cache => {
+                cache.put(event.request , clone);
+            });
+            return res;
+        }).catch(e => caches.match(event.request).then(res => res))
+    );
+});
